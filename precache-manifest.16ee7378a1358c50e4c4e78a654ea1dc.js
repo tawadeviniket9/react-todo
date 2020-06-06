0cc4b7ebe8525c92786429952f738bf9619db33a
Viniket9 <61202981+Viniket9@users.noreply.github.com>
2020-06-06 16:43:54 +0530

    Updates

---
+++ precache-manifest.16ee7378a1358c50e4c4e78a654ea1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6411521dff470313654af82cdd9c6280",
+    "url": "/react-todo.git/index.html"
+  },
+  {
+    "revision": "460cd6f4de65ac839012",
+    "url": "/react-todo.git/static/css/main.4d4aae80.chunk.css"
+  },
+  {
+    "revision": "9cc1006b396172dd6b00",
+    "url": "/react-todo.git/static/js/2.e4a41437.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo.git/static/js/2.e4a41437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460cd6f4de65ac839012",
+    "url": "/react-todo.git/static/js/main.c3d9f265.chunk.js"
+  },
+  {
+    "revision": "63ea07b8c39e1f52c003",
+    "url": "/react-todo.git/static/js/runtime-main.82452e6f.js"
+  }
+]);
